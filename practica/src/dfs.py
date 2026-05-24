f4746f3dd30405ce99284dfa1677cd61c40456e0
JhosuaS <jhosua_saa2412@hotmail.com>
2026-05-23 19:58:04 -0500

    added dfs.py
    modified the dfs algorithm reviewed in class to work with OOP

--- practica/src/dfs.py
+++ practica/src/dfs.py
@@ -1,13 +1,38 @@
-def dfs_tree(nodo, grafo, searched, componentR, arbolDfs):
-    componentR.append(nodo)
-    searched[nodo] = True
-    
-    for vecino in grafo[nodo]:
-        if not searched[vecino]:
-            arbolDfs[vecino] = nodo
-            dfs_tree(vecino, grafo, searched, componentR, arbolDfs)
-            print("Finaliza", vecino)
-            print("Vuelve a", nodo)
-            print()
+def dfs(grafo_obj, nodo_actual: str, nodo_fin: str, visitados=None, camino=None, arbol_dfs=None):
+    """
+    Algoritmo DFS recursivo integrado con POO.
+    Devuelve una tupla: (camino_encontrado, arbol_dfs)
+    """
+    if (nodo_actual not in grafo_obj.nodos) or (nodo_fin not in grafo_obj.nodos) :
+        print(f"Error el nodo {nodo_actual} y/o el nodo {nodo_fin} no existen en el grafo")
+        return None, None
 
-    return arbolDfs
+    if visitados is None:
+        visitados = []
+        camino = []
+        arbol_dfs = {}
+        print("\n--- Iniciando Evacuación (DFS Recursivo) ---")
+
+    visitados.append(nodo_actual)
+    camino.append(nodo_actual)
+    print(f"Visitando: {nodo_actual}") 
+
+    # 3. Caso Base: ¡Encontramos el punto de encuentro!
+    if nodo_actual == nodo_fin:
+        return camino, arbol_dfs
+
+    arbol_dfs[nodo_actual] = [] 
+
+    for arista in grafo_obj.adyacencias[nodo_actual]:
+        vecino = arista.destino.nombre
+
+        if vecino not in visitados:
+            arbol_dfs[nodo_actual].append(vecino)
+
+            resultado_camino, resultado_arbol = dfs(grafo_obj, vecino, nodo_fin, visitados, camino, arbol_dfs)
+            if resultado_camino is not None:
+                return resultado_camino, resultado_arbol
+
+    camino.pop()
+    
+    return None, arbol_dfs
