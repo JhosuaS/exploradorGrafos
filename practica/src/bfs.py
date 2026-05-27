9a78a93b46db6808ce2e0946290dad49502f8ca0
edisonogonaga <151202618+edisonogonaga@users.noreply.github.com>
2026-05-26 20:20:29 -0500

    Update bfs.py

--- practica/src/bfs.py
+++ practica/src/bfs.py
@@ -12,7 +12,7 @@ def ejecutar_bfs(grafo_obj, start: str, finish: str):
     cola = deque([(start, [start])])
     visitados = {start}
     
-    # 1. Inicializamos el diccionario que guardará el árbol BFS
+
     arbol_bfs = {}
     
     print("\n--- Iniciando Evacuación (BFS) ---")
@@ -22,14 +22,14 @@ def ejecutar_bfs(grafo_obj, start: str, finish: str):
         orden_visita.append(nodo_actual)
         print(f"Visitando: {nodo_actual}")
 
-        # 2. Preparamos al nodo actual en el árbol para poder anotarle sus hijos
+    
         if nodo_actual not in arbol_bfs:
             arbol_bfs[nodo_actual] = []
 
         # Caso Base: Llegamos al destino
         if nodo_actual == finish:
             pasos = len(camino) - 1
-            # 3. Agregamos arbol_bfs a los valores de retorno
+  
             return orden_visita, camino, pasos, True, arbol_bfs
 
         # Explorar vecinos
@@ -40,8 +40,7 @@ def ejecutar_bfs(grafo_obj, start: str, finish: str):
                 visitados.add(vecino)
                 cola.append((vecino, camino + [vecino]))
                 
-                # 4. Registramos al vecino como hijo del nodo actual en nuestro árbol
+ 
                 arbol_bfs[nodo_actual].append(vecino)
 
-    # Si no encuentra ruta, también devuelve el árbol de lo que alcanzó a explorar
     return orden_visita, None, 0, False, arbol_bfs
